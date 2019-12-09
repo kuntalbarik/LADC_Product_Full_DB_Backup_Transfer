dfd80a3e948af01f1596b16e2ede8316fd0c72ca
kuntalbarik <50690632+kuntalbarik@users.noreply.github.com>
2019-12-09 18:08:44 +0530

    updated code for production
    If folder is empty, code is breaking.Need to resolve the code for the case

--- LADC_Product_Full_DB_Backup_Transfer/Full_DB_Backup_Transfer.py
+++ LADC_Product_Full_DB_Backup_Transfer/Full_DB_Backup_Transfer.py
@@ -1,15 +1,16 @@
 import os, glob, time, operator
-from datetime import timedelta,date
-import datetime,shutil
+from datetime import date,datetime,timedelta
+import shutil
 
 def modification_date(filename):
     t = os.path.getmtime(filename)
-    modifiedDate=str(datetime.datetime.fromtimestamp(t))
+    modifiedDate=str(datetime.fromtimestamp(t))
     modifiedDate=modifiedDate[0:4]+'_'+modifiedDate[5:7]+'_'+modifiedDate[11:13]
     return  modifiedDate
 
 ###245 GB=263066746880 Byte
 ###500 MB=524288000 Byte
+maxFolderSizeCanBeArchived=263066746880
 def convert_bytes(num):
     """
     this function will convert bytes to MB.... GB... etc
@@ -58,56 +59,112 @@ def get_oldest_file(files, _invert=False):
     # Return just the name of oldest file.
     return oldest[0]
 
+##log path
+logPath="c:\\Full_DB_Archival_History"
+if (os.path.exists(logPath) == False):
+    os.mkdir(logPath)
+##creating log file
+now=datetime.now()
+log_date=str(now)
+filepath=logPath+"\\log_"+log_date[0:11] + '.txt'
+fileobject=open(filepath,"a+")
+fileobject.close()
 
-backupPath=r"D:\laxisilon.pftla.local\Clear\ifs\PRODUCTION_BACKUPS\SQLDB\Full_Production_DB_Backup_Products"
-#backupPath1=backupPath
 
+#backupPath=r"D:\laxisilon.pftla.local\Clear\ifs\PRODUCTION_BACKUPS\SQLDB\Full_Production_DB_Backup_Products"
+backupPath=r"\\laxisilon.pftla.local\Clear\ifs\PRODUCTION_BACKUPS\SQLDB\Full_Production_DB_Backup_Products"
+
+fileobject=open(filepath,"a+")
 for roots, dirs, files in os.walk(backupPath):
     for dir in dirs:
         backupPath1= backupPath
         backupPath1+="\\"+dir
-        fileSizeMoved=0
+        fileSizeMoved=noOfFilesToMove=fileSizeToMoveInByte=startDate=endDate=0
         filesToMove=[]
-        noOfFilesToMove=0
-        fileSizeToMoveInByte=0
         days=getDates()
-        startDate=0
-        endDate=0
-        condition=True
-        for roots,dirs,files in os.walk(backupPath1):
-           files = glob.glob(backupPath1 + '\\*.bak')
-           oldestFile = get_oldest_file(files)
-           if ("_01_01_" in (oldestFile) or "_03_01_" in (oldestFile) or
+        directoryNameLength = len(dir)
+        ###Aspera hot folder Path
+        moveToTemp1 = r"\\laxisilon.pftla.local\Clear\ifs\PRODUCTION_BACKUPS\LA-To-TN-ProdBackup\SQL DB\\"
+        ###creating the sub folder in side aspera hot folder
+        moveToTemp = r"\\laxisilon.pftla.local\Clear\ifs\PRODUCTION_BACKUPS\LA-To-TN-ProdBackup\SQL DB\\" + dir
+
+        if (os.path.isdir(moveToTemp) == False):
+            os.mkdir(moveToTemp)
+
+        ###getting the oldest file and its size
+        files = glob.glob(backupPath1 + '\\*.bak')
+        oldestFile = get_oldest_file(files)
+        ###if individual file sixe gater than 245 GB
+        if(GetFileSize(oldestFile)>maxFolderSizeCanBeArchived):
+            tempFileName = oldestFile[94 + directoryNameLength::]
+            modificationDate = modification_date(oldestFile)
+            if(modificationDate not in days):
+                filesToMove.append(oldestFile)
+                fileSizeToMoveInByte += GetFileSize(oldestFile)
+                noOfFilesToMove += 1
+                shutil.move(oldestFile, moveToTemp + "\\" + tempFileName)
+                startFile = filesToMove[0]
+                endFile = filesToMove[len(filesToMove) - 1]
+                finalFoleder = (dir + "_full_backup_" + startFile[94 + (2 * len(dir)) + 9:94 + (
+                    2 * len(dir)) + 19] + "_to_" +
+                            endFile[94 + (2 * len(dir)) + 9:94 + (2 * len(dir)) + 19])
+                os.rename(moveToTemp, moveToTemp1 + finalFoleder)
+                message=("{} dropped on :-{} folder size :-{}\n".format(finalFoleder, str(date.today()),
+                                                               convert_bytes(fileSizeToMoveInByte)))
+                print(message)
+                fileobject.write(message)
+            else:
+                message=tempFileName+"is not 30 days older\n"
+                print(message)
+                fileobject.write(message)
+
+        else:
+            ###if month has 31 days max 31 files can be clubbed in 1 folder
+            if ("_01_01_" in (oldestFile) or "_03_01_" in (oldestFile) or
                     "_05_01_" in (oldestFile) or "_07_01_" in (oldestFile) or
                     "_08_01_" in (oldestFile) or "_10_01_" in (oldestFile) or
                     "_12_01_" in (oldestFile)):
                 maxIterate = 31
-           elif ("_02_01_" in oldestFile):
+            ###if month is feb check leap year and decide max number of files can be clubbed in single folder
+            elif ("_02_01_" in oldestFile):
                 maxIterate = 28
-                if (int(oldestFile[len(dir) + 8:len(dir) + 12]) % 4 == 0):
-                    maxIterate=29
-           else:
+                if (int(oldestFile[94+(2*len(dir))+9:94+(2*len(dir))+13]) % 4 == 0):
+                    maxIterate = 29
+            ###if month has 30 days max 30 files can be clubbed in 1 folder
+            else:
                 maxIterate = 30
-           for i in range(maxIterate):
-                files = glob.glob(backupPath1+'\\*.bak')
-                oldestFile=get_oldest_file(files)
-                directoryNameLength=len(dir)
-                tempFileName=oldestFile[95+directoryNameLength::]
-                moveToTemp1=r"D:\filesToMoveToTN\\"
-                moveToTemp=r"D:\filesToMoveToTN\\"+dir
-                if(os.path.isdir(moveToTemp)==False):
-                    os.mkdir(moveToTemp)
-                modificationDate=modification_date(oldestFile)
-                if( modificationDate not in(days)and noOfFilesToMove<maxIterate and fileSizeToMoveInByte<=263066746880):
-                    filesToMove.append(oldestFile)
-                    fileSizeToMoveInByte+=GetFileSize(oldestFile)
-                    noOfFilesToMove+=1
-                    shutil.move(oldestFile,moveToTemp+"\\"+tempFileName)
-                if (noOfFilesToMove == maxIterate):
-                    startFile=filesToMove[0]
-                    endFile=filesToMove[len(filesToMove)-1]
-                    finalFoleder=(dir+"_full_backup_"+startFile[95+(2*len(dir))+9:95+(2*len(dir))+19]+"_to_"+
-                          endFile[95+(2*len(dir))+9:95+(2*len(dir))+19])
-                    os.rename(moveToTemp,moveToTemp1+finalFoleder)
-                    print("{} dropped on :-{} folder size :-{}".format(finalFoleder,str(date.today()),convert_bytes(fileSizeToMoveInByte)))
-                    break
+
+            condition=True
+            ###for all the file in the sub folder checking the file
+            for roots, dirs, files in os.walk(backupPath1):
+                while condition:
+                    for i in range(maxIterate):
+                        files = glob.glob(backupPath1 + '\\*.bak')
+                        oldestFile = get_oldest_file(files)
+                        tempFileName = oldestFile[94 + directoryNameLength::]
+                        modificationDate = modification_date(oldestFile)
+                        if (
+                                modificationDate not in days and noOfFilesToMove < maxIterate and fileSizeToMoveInByte <= maxFolderSizeCanBeArchived):
+                            filesToMove.append(oldestFile)
+                            fileSizeToMoveInByte += GetFileSize(oldestFile)
+                            noOfFilesToMove += 1
+                            shutil.move(oldestFile, moveToTemp + "\\" + tempFileName)
+                        else:
+                            condition=False
+                            break
+
+                    if condition==False:
+                        startFile = filesToMove[0]
+                        endFile = filesToMove[len(filesToMove) - 1]
+                        finalFoleder = (dir + "_full_backup_" + startFile[94 + (2 * len(dir)) + 9:94 + (
+                                    2 * len(dir)) + 19] + "_to_" +
+                                            endFile[94 + (2 * len(dir)) + 9:94 + (2 * len(dir)) + 19])
+                        os.rename(moveToTemp, moveToTemp1 + finalFoleder)
+                        message="{} dropped on :-{} folder size :-{}\n".format(finalFoleder, str(date.today()),
+                                                                               convert_bytes(fileSizeToMoveInByte))
+                        print(message)
+                        fileobject.write(message)
+                        break
+message="-----------------------------------------------------------------------------------------------------------------------\n"
+fileobject.write(message)
+fileobject.close()
